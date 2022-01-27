3593f6e4bb63eee99c63944b0fe1e074ce8b72b2
Anastasia <anastasiyaaki171@gmail.com>
2022-01-27 12:19:08 +0300

    Initial commit

---
+++ src/test/java/by/akimova/userAuthApp/UserAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.akimova.userAuthApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
